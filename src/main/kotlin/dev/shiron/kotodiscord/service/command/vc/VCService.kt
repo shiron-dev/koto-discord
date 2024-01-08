e26e93aa0a3c2ec6f47ec8c301ffbf2efc842dce
shiron <shiron4710.dev@gmail.com>
2024-01-08 21:17:26 +0900

    feat(command): update vc_notification service
    embed

--- src/main/kotlin/dev/shiron/kotodiscord/service/command/vc/VCService.kt
+++ src/main/kotlin/dev/shiron/kotodiscord/service/command/vc/VCService.kt
@@ -2,11 +2,13 @@ package dev.shiron.kotodiscord.service.command.vc
 
 import dev.shiron.kotodiscord.domain.VCNotificationData
 import dev.shiron.kotodiscord.repository.VCNotificationDataRepository
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.MessageSource
 import org.springframework.stereotype.Service
+import java.text.SimpleDateFormat
 import java.util.*
 
 @Service
@@ -31,20 +33,52 @@ class VCService
         override fun onGuildVoiceUpdate(event: GuildVoiceUpdateEvent) {
             val guild = event.guild
             val allData = vcRepository.findAllByGuildId(guild.idLong) ?: return
+            val textChannelList = mutableListOf<Long>()
 
-            if (event.channelJoined != null) {
-                val joinData = allData.filter { it.vcChannelId == event.channelJoined?.idLong || it.vcCategoryId == event.channelJoined?.parentCategoryIdLong || (it.vcCategoryId == null && it.vcChannelId == null) }
+            for (data in allData) {
+                val isJoin =
+                    event.channelJoined != null && (
+                        data.vcChannelId == event.channelJoined?.idLong ||
+                            data.vcCategoryId == event.channelJoined?.parentCategoryIdLong
+                    )
+                val isLeft =
+                    event.channelLeft != null && (
+                        data.vcChannelId == event.channelLeft?.idLong ||
+                            data.vcCategoryId == event.channelLeft?.parentCategoryIdLong
+                    )
+                val isAll = data.vcChannelId == null && data.vcCategoryId == null
 
-                for (data in joinData) {
-                    guild.getTextChannelById(data.textChannelId)?.sendMessage(messages.getMessage("service.message.vc_notification.join", arrayOf(event.member.asMention, data.vcName), Locale.JAPAN))?.queue()
+                if (isJoin || isLeft || isAll) {
+                    textChannelList.add(data.textChannelId)
                 }
             }
-            if (event.channelLeft != null) {
-                val leftData = allData.filter { it.vcChannelId == event.channelLeft?.idLong || it.vcCategoryId == event.channelLeft?.parentCategoryIdLong || (it.vcCategoryId == null && it.vcChannelId == null) }
 
-                for (data in leftData) {
-                    guild.getTextChannelById(data.textChannelId)?.sendMessage(messages.getMessage("service.message.vc_notification.left", arrayOf(event.member.asMention, data.vcName), Locale.JAPAN))?.queue()
-                }
+            val eb = EmbedBuilder()
+            eb.setAuthor(event.member.effectiveName, null, event.member.effectiveAvatarUrl)
+
+            val date = Date()
+            val timeZoneJP = TimeZone.getTimeZone("Asia/Tokyo")
+            val fmt = SimpleDateFormat()
+            fmt.timeZone = timeZoneJP
+            eb.setFooter(fmt.format(date))
+
+            val isJoin = event.channelJoined != null
+            val isLeft = event.channelLeft != null
+            if (isJoin && isLeft) {
+                eb.setTitle("Change")
+                eb.setDescription(messages.getMessage("service.message.vc_notification.change", arrayOf(event.member.asMention, event.channelLeft?.asMention, event.channelJoined?.asMention), Locale.JAPAN))
+            } else if (isJoin) {
+                eb.setTitle("Join")
+                eb.setDescription(messages.getMessage("service.message.vc_notification.join", arrayOf(event.member.asMention, event.channelJoined?.asMention), Locale.JAPAN))
+            } else if (isLeft) {
+                eb.setTitle("Left")
+                eb.setDescription(messages.getMessage("service.message.vc_notification.left", arrayOf(event.member.asMention, event.channelLeft?.asMention), Locale.JAPAN))
             }
+
+            for (textChannel in textChannelList) {
+                guild.getTextChannelById(textChannel)?.sendMessage("")?.setEmbeds(eb.build())?.queue()
+            }
+
+            // guild.getTextChannelById(data.textChannelId)?.sendMessage(messages.getMessage("service.message.vc_notification.join", arrayOf(event.member.asMention, data.vcName), Locale.JAPAN))?.queue()
         }
     }
