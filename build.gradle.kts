d1a4acb8dd3b246d6e70de64bcd08fe9ba244812
shiron <shiron4710.dev@gmail.com>
2024-03-30 16:26:06 +0900

    feat(deps): add spring-boot-starter-actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ ktlint {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
