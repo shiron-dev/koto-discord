2f27ed1c63e28c738a34341d6a0af4a8a370944a
shiron <shiron4710.dev@gmail.com>
2024-03-30 15:57:15 +0900

    feat: add bump command

---
+++ src/main/kotlin/dev/shiron/kotodiscord/domain/BumpConfigData.kt
@@ -0,0 +1,15 @@
+package dev.shiron.kotodiscord.domain
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "bump_config")
+data class BumpConfigData(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Long = 0,
+    val guildId: Long,
+    val channelId: Long,
+    val mentionId: Long?,
+)
--- src/main/kotlin/dev/shiron/kotodiscord/util/meta/SingleCommandEnum.kt
+++ src/main/kotlin/dev/shiron/kotodiscord/util/meta/SingleCommandEnum.kt
@@ -4,4 +4,5 @@ enum class SingleCommandEnum(val metadata: RunnableCommandMeta) {
     HELLO(RunnableCommandMeta("hello")),
     HELP(RunnableCommandMeta("help")),
     ABOUT(RunnableCommandMeta("about")),
+    BUMP(RunnableCommandMeta("notify_bump")),
 }
