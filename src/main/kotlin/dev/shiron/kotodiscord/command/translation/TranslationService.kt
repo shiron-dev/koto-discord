1a29b4c3f50006073bed3121afa146fea063cf25
shiron <shiron4710.dev@gmail.com>
2024-04-19 13:33:48 +0900

    fix: trans only first reaction

--- src/main/kotlin/dev/shiron/kotodiscord/command/translation/TranslationService.kt
+++ src/main/kotlin/dev/shiron/kotodiscord/command/translation/TranslationService.kt
@@ -3,6 +3,7 @@ package dev.shiron.kotodiscord.command.bump
 import dev.shiron.kotodiscord.command.translation.translateText
 import dev.shiron.kotodiscord.vars.properties.AppProperties
 import kotlinx.coroutines.runBlocking
+import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import org.springframework.beans.factory.annotation.Autowired
@@ -23,15 +24,22 @@ class TranslationService
     ) : ListenerAdapter() {
         override fun onMessageReactionAdd(event: MessageReactionAddEvent) =
             runBlocking {
+                if (event.user?.isBot == true) return@runBlocking
+                if (event.reaction.isSelf) return@runBlocking
+
                 val reaction = event.reaction.emoji.name
 
                 val transCode = TranslationReaction.values().find { it.reactionEmoji == reaction } ?: return@runBlocking
-                val count = event.reaction.count
-
-                if (count < 2) return@runBlocking
 
                 val channel = event.guild.getTextChannelById(event.channel.id) ?: return@runBlocking
                 val message = channel.retrieveMessageById(event.messageId).complete() ?: return@runBlocking
+
+                val msgReaction = message.getReaction(Emoji.fromUnicode(transCode.reactionEmoji)) ?: return@runBlocking
+                if (msgReaction.count != 1) {
+                    return@runBlocking
+                }
+
+                message.addReaction(Emoji.fromUnicode(transCode.reactionEmoji)).queue()
 
                 val apiKey = appProperties.deeplApiKey ?: return@runBlocking
                 val sourceText = message.contentRaw
